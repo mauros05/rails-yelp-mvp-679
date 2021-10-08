d5615fd050a1b9d299f5c5c91893526d6d9d4e4a
Eduardo Gutierrez <emaurogtz@hotmail.es>
2021-10-08 10:58:15 -0500

    Ex2. finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  rand(1..5).times do
+    Review.create(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review,
+      restaurant: restaurant
+    )
+  end
+end
